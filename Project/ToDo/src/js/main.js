f8fdd79ab3df4305f3b843b4da509a1126b31568
santhanakrishnanstark <santhanakrishnanstark@gmail.com>
2024-08-27 21:44:24 +0530

    todo edit mode added

--- Project/ToDo/src/js/main.js
+++ Project/ToDo/src/js/main.js
@@ -56,6 +56,10 @@ function renderTodoItem(todoItem) {
     const liTag = document.createElement("li");
     liTag.classList.add("todo-item");
 
+    if (todoItem.isCompleted) {
+        liTag.classList.add("completed");
+    }
+
     const todoItemHTML = getTodoItemHTML(todoItem);
 
     liTag.innerHTML = todoItemHTML;
@@ -66,30 +70,82 @@ function renderTodoItem(todoItem) {
 function getTodoItemHTML(todoItem) {
     return `
         <p class="todo-check">
-            ${
-                todoItem.isCompleted 
-                ? `<input type="checkbox" name="todo-check" id="todo-check-${todoItem.id}" checked>`
-                : `<input type="checkbox" name="todo-check" id="todo-check-${todoItem.id}">`
-            }
+            ${todoItem.isCompleted
+            ? `<input type="checkbox" name="todo-check" id="todo-check-${todoItem.id}" checked disabled>`
+            : `<input type="checkbox" name="todo-check" id="todo-check-${todoItem.id}">`
+        }
         </p>
         <div class="todo-content">
-            <button class="todo-menu-btn icon-prefix dotted-menu-icon"></button>
+            <button class="todo-menu-btn icon-prefix dotted-menu-icon" onclick="toggleMenu(event)"></button>
             <ul class="todo-menu-list">
-                <li><a href="#" class="todo-menu icon-prefix edit-icon">Edit</a></li>
-                <li><a href="#" class="todo-menu icon-prefix delete-icon">Delete</a></li>
+                <li><a href="#" class="todo-menu icon-prefix edit-icon" onclick="handleEditMode(event,  ${todoItem.id})">Edit</a></li>
+                <li><a href="#" class="todo-menu icon-prefix delete-icon" onclick="handleDelete(event)">Delete</a></li>
             </ul>
-            <p class="todo-title">${todoItem.todoText}</p>
+            <p class="todo-title"><span>${todoItem.todoText}</span></p>
             <p class="todo-created-date">Created at: ${todoItem.createdAt}</p>
 
             ${getTodoItemStatus(todoItem.isCompleted)}
 
             <p class="todo-action-btn">
                 <button class="button button-primary button-primary-outline icon-prefix mark-icon ${todoItem.isCompleted ? 'completed' : ''}" onClick="markTodoAsComplete(event, ${todoItem.id})">
-                    ${todoItem.isCompleted ? "Undo Completed" : "Mark Complete" }
+                    ${todoItem.isCompleted ? "Undo Completed" : "Mark Complete"}
                 </button>
             </p>
         </div>
     `;
+}
+
+function toggleMenu(event) {
+    const menuList = event.target.nextElementSibling;
+    if (menuList.classList.contains("show")) {
+        menuList.classList.remove("show");
+    } else {
+        menuList.classList.add("show");
+    }
+}
+
+function handleEditMode(event, id) {
+    event.preventDefault();
+
+    const currentItem = event.target.closest(".todo-item");
+
+    const textInput = document.createElement("input");
+    textInput.type = "text";
+    textInput.setAttribute("data-id", id);
+
+    const currentTitleEl = currentItem.querySelector(".todo-title");
+
+    // get the previous todo title text and assign it to the text input
+    textInput.value = currentTitleEl.querySelector("span").textContent;
+
+    // Add event listener to the textInput, On enter press Update the todo Item.
+    textInput.addEventListener("keyup", (event) => {
+        if (event.code === "Enter") {
+            const updatedTitle = event.target.value;
+            const currentId = event.target.getAttribute("data-id");
+
+            todoList[currentId].todoText = updatedTitle;
+
+            // Update the localStorage
+            localStorage.setItem("TODOLIST", JSON.stringify(todoList));
+
+            // Update the span tag value
+            event.target.previousElementSibling.textContent = updatedTitle;
+
+            event.target.parentElement.classList.remove("edit-mode");
+            event.target.remove();
+
+        }
+    });
+
+    currentTitleEl.classList.add("edit-mode");
+
+    currentTitleEl.appendChild(textInput);
+
+    // After rendering the text input, close the menu
+
+    currentItem.querySelector(".todo-menu-list").classList.remove("show");
+
 }
 
 // <button class="todo-menu-btn icon-prefix dotted-menu-icon"></button>
@@ -142,7 +198,12 @@ function markTodoAsComplete(event, id) {
 
     // Update the Local UI
     const currentListEl = markBtn.closest(".todo-item");
+
+    // selecting / deselecting the checkbox based on the completed status
     currentListEl.querySelector(`#todo-check-${id}`).checked = isCompleted;
+
+    // disabling / enabling the checkbox based on the completed status
+    currentListEl.querySelector(`#todo-check-${id}`).disabled = isCompleted;
 
     const badgeEl = currentListEl.querySelector(`.todo-status`);
     if (isCompleted) {
@@ -152,6 +213,10 @@ function markTodoAsComplete(event, id) {
 
         // update the mark button text
         markBtn.textContent = "Undo Completed";
+
+        // add class "completed" to the parent class todo-item 
+        markBtn.closest(".todo-item").classList.add("completed");
+
     } else {
         badgeEl.classList.add('warning');
         badgeEl.classList.remove('success');
@@ -159,6 +224,9 @@ function markTodoAsComplete(event, id) {
 
         // update the mark button text
         markBtn.textContent = "Mark Completed";
+
+        // remove the class "completed" to the parent class todo-item 
+        markBtn.closest(".todo-item").classList.remove("completed");
     }
 
 }
